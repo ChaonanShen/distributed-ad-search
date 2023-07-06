7d2cccacde931cdef8e6b2310ca8ce1a51cff5d7
ChaonanShen <1686745041@qq.com>
2023-07-06 04:35:55 +0000

    balancer中复用channel和stub，而不是每次重新生成

--- code/alimama/balancer.cc
+++ code/alimama/balancer.cc
@@ -21,28 +21,46 @@ using alimama::proto::SearchService;
 
 // server1 ip1:50051 server2 ip2:50052 server3 ip3:50053
 static std::string searchServerAddrs[3];
+static std::unique_ptr<SearchService::Stub> stubs[3];
+
+// 创建一个etcd客户端
+etcd::Client etcd_client("http://etcd:2379");
 
 class LoadBalancerImpl final : public SearchService::Service {
   Status Search(ServerContext *context, const Request *request,
                 Response *response) override {
-
     // 轮流转发给不同的机器
     static int i = 0;
-    std::string server_address = searchServerAddrs[i];
     i = (i + 1) % 3;
 
-    std::unique_ptr<SearchService::Stub> stub(
-        SearchService::NewStub(grpc::CreateChannel(
-            server_address, grpc::InsecureChannelCredentials())));
-
     grpc::ClientContext client_context;
-    auto status = stub->Search(&client_context, *request, response);
+    auto status = stubs[i]->Search(&client_context, *request, response);
     if (!status.ok()) {
-      std::cout << "balancer receive response RPC failed" << std::endl;
+      // TODO(scn): 如果检测到是channel状态出问题，就重新更换！
+      std::cout << "balancer receive response RPC Failed" << std::endl;
     }
     return status;
   }
 };
+
+void prepare() {
+  // 等待三个server将数据都准备好
+  std::string dummy_str;
+  splitStr(EtcdGetKVWait(etcd_client, "/node1"), searchServerAddrs[0],
+           dummy_str);
+  splitStr(EtcdGetKVWait(etcd_client, "/node2"), searchServerAddrs[1],
+           dummy_str);
+  splitStr(EtcdGetKVWait(etcd_client, "/node3"), searchServerAddrs[2],
+           dummy_str);
+
+  // rpc一直复用一个连接，不需要每次都生成！
+  stubs[0] = SearchService::NewStub(grpc::CreateChannel(
+      searchServerAddrs[0], grpc::InsecureChannelCredentials()));
+  stubs[1] = SearchService::NewStub(grpc::CreateChannel(
+      searchServerAddrs[1], grpc::InsecureChannelCredentials()));
+  stubs[2] = SearchService::NewStub(grpc::CreateChannel(
+      searchServerAddrs[2], grpc::InsecureChannelCredentials()));
+}
 
 void RunLoadBalancer() {
   std::string local_ip = getLocalIP();
@@ -53,7 +71,6 @@ void RunLoadBalancer() {
   std::string external_address =
       local_ip + std::string(":") + std::to_string(kPort);
   std::string server_address("0.0.0.0:" + std::to_string(kPort));
-  std::string key = std::string("/services/searchservice");
 
   LoadBalancerImpl service;
   ServerBuilder builder;
@@ -62,19 +79,10 @@ void RunLoadBalancer() {
 
   std::unique_ptr<Server> server(builder.BuildAndStart());
 
-  // TODO(scn)： 本地测试和线上测试这里要修改
-  // 创建一个etcd客户端
-  etcd::Client etcd("http://etcd:2379");
-
-  // 等待三个server将数据都准备好再注册
-  std::string dummy_str;
-  splitStr(EtcdGetKVWait(etcd, "/node1"), searchServerAddrs[0], dummy_str);
-  splitStr(EtcdGetKVWait(etcd, "/node2"), searchServerAddrs[1], dummy_str);
-  splitStr(EtcdGetKVWait(etcd, "/node3"), searchServerAddrs[2], dummy_str);
-
   // 将服务地址注册到etcd中
   // 相当于 etcdctl put /services/searchservice ip:port
-  auto response = etcd.set(key, external_address).get();
+  std::string key = std::string("/services/searchservice");
+  auto response = etcd_client.set(key, external_address).get();
   if (response.is_ok()) {
     std::cout << "Service registration successful.\n";
   } else {
@@ -87,6 +95,7 @@ void RunLoadBalancer() {
 }
 
 int main() {
+  prepare();
   RunLoadBalancer();
 
   return 0;
